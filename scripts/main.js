a334139331c137d0e3fadbae883ec99700107324
Gabriel Desjaridns <desjardins.gabriel@gmail.com>
2012-06-14 23:56:33 -0400

    now with touchSwipe

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,17 @@ $(document).ready(function(){
 });
 $(window).load(function(){
 	$('.carousel').carousel({interval:false});
+	$(".carousel").swipe({
+		threshold:0,
+		swipe: function(event, direction){
+			if (direction === 'left') {
+				$('.carousel').carousel('next');
+			}
+			else if (direction === 'right') {
+				$('.carousel').carousel('prev');
+			}}
+	});
+	/*
 	$('.carousel img').on('touchy-swipe', function(event, $target, data){
 			var direction = data.direction;
 			//console.log(direction);
@@ -13,4 +24,5 @@ $(window).load(function(){
 				$('.carousel').carousel('prev');
 			}
 	});
+	*/
 });
