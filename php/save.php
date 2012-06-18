c9aa66b2ab2cdd715f81fb75c7b185f8ba62df9d
Gabriel Desjaridns <desjardins.gabriel@gmail.com>
2012-06-18 17:11:03 -0400

    1.0.8

---
+++ php/save.php
@@ -0,0 +1,8 @@
+﻿<?php
+$value = json_decode($_POST['json']);
+$myFile = "testFile.txt";
+$fh = fopen($myFile, 'w') or die("can't open file");
+fwrite($fh, $_POST['json']);
+fclose($fh);
+echo $value->currentScrn;
+?>
